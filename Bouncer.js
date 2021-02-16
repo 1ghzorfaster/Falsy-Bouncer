33f34a3c89cb0f0dd241ea14f826b0704044e2b3
Jessica Dube <76919568+1ghzorfaster@users.noreply.github.com>
2021-02-15 18:40:27 -0800

    Update Bouncer.js

--- Bouncer.js
+++ Bouncer.js
@@ -7,7 +7,3 @@ function bouncer(arr) {
     }
   return arr;
 }
-
-console.log(bouncer([7, "ate", "", false, 9]));
-console.log(bouncer([false, null, 0, NaN, undefined, ""]));
-console.log(bouncer([null, NaN, 1, 2, undefined]));
